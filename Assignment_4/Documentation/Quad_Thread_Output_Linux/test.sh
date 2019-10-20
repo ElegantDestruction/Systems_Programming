437591c3366b6035eac4032d9e511ce2b2001dbd
ElegantDestruction <jhillebrand25@gmail.com>
2019-10-20 16:45:55 -0500

    Completed Assignment 4

---
+++ Assignment_4/Documentation/Quad_Thread_Output_Linux/test.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+echo 'Beginning Run 1'
+
+./OS_Assignment_4.out > run_1.txt
+
+echo 'Beginning Run 2'
+
+./OS_Assignment_4.out > run_2.txt
+
+echo 'Beginning Run 3'
+
+./OS_Assignment_4.out > run_3.txt
+
+echo 'Beginning Run 4'
+
+./OS_Assignment_4.out > run_4.txt
+
+echo 'Complete!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
---
+++ Assignment_4/Documentation/Sixteen_Thread_Output_Win/run_test.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+START /B /wait OS_Assignment_4.exe>run_1.txt
+START /B /wait OS_Assignment_4.exe>run_2.txt
+START /B /wait OS_Assignment_4.exe>run_3.txt
+START /B /wait OS_Assignment_4.exe>run_4.txt
+
